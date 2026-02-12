ca77d635527e93cb39e5d8a931c11e8f4e9c07a2
Lemuel567 <lemuelbenyarku11@gmail.com>
2026-02-12 00:36:58 +0000

    Add structural integrity simulation

---
+++ question3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class question3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double structuralIntegrity = 100.0;
+        int cycleCount = 0;
+        final double FAILURE_THRESHOLD = 15.0;
+
+        while(structuralIntegrity > FAILURE_THRESHOLD) {
+            System.out.print("Enter pressure force for Cycle " + (cycleCount + 1));
+            double pressure = input.nextDouble();
+
+
+            if (pressure > 0) {
+                structuralIntegrity -= pressure;
+                if (pressure > 20.0) {
+
+                    structuralIntegrity -= 5;
+
+                }
+                cycleCount++;
+                System.out.println("The remaining integrity is: " + structuralIntegrity);
+
+            }
+
+            else{
+                System.out.println("pressure can only be positive");
+            }
+        }
+            System.out.println("Material failure at cycle " + cycleCount);
+            System.out.println("The final integrity level is: " + structuralIntegrity);
+
+
+
+    }
+
+}
